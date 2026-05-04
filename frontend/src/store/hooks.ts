0c0fe25572a573a4c4fcf747f64e0dd1b6a40a45
Luciano <barberisluciano58@gmail.com>
2026-05-04 16:21:40 -0300

    add ReduxToolkit & store configuration

---
+++ frontend/src/store/hooks.ts
@@ -0,0 +1,8 @@
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+import type { RootState, AppDispatch } from './index';
+
+// Hook tipado para useDispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+// Hook tipado para useSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
