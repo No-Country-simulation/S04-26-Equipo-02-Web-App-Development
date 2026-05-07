a23e998ef2d750b63a87a93b738e3f6a6d52a68c
Gonzalo Travieso <104102194+zhark34@users.noreply.github.com>
2026-05-07 11:58:28 -0300

    agregado rutas para verificación de correo electrónico y validación de sesión

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -5,12 +5,21 @@ const router = Router();
 import {
 
     loginController,
-    registerController
+    registerController,
+    verifyEmailController,
+    validateSessionController
 
 } from '../controllers/auth.controller';
+
+import { refreshToken } from '../middlewares/refresh.token';
+import { tokenMiddleware } from '../middlewares/token.middlewares';
 
 router.post("/login", loginController);
 
 router.post("/register", registerController);
+
+router.patch("/verify-email/:token", verifyEmailController);
+
+router.get("/validate-session", refreshToken, tokenMiddleware, validateSessionController);
 
 export default router;
