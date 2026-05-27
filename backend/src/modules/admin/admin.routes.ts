687c3c33ca510e592e3d604fb481640d673f5469
Gonzalo Travieso <traviesogonzalo@gmail.com>
2026-05-27 12:21:41 -0300

    feat: Agregar rutas para la creación de administradores con middleware de autenticación

---
+++ backend/src/modules/admin/admin.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import * as AdminRoutes from './admin.controller';
+
+import { refreshToken } from '../../middlewares/refresh.token.middleware';
+import { tokenMiddleware } from '../../middlewares/token.middleware';
+
+const router = Router();
+
+router.post('/create', tokenMiddleware, refreshToken, AdminRoutes.createAdmin);
+
+export default router;
