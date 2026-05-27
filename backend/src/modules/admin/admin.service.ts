069d1167959acce2542fb9a9558ddec8813f6f4d
Gonzalo Travieso <traviesogonzalo@gmail.com>
2026-05-27 12:21:53 -0300

    feat: Implementar servicio para la creación de administradores con validación de correo y envío de notificación

---
+++ backend/src/modules/admin/admin.service.ts
@@ -0,0 +1,38 @@
+import { prisma } from '../../utils/prisma';
+import { Role } from '@prisma/client';
+import { generateAdminPassword } from '../../utils/generate.password.admin';
+import { sendEmail } from '../../config/nodemailer';
+
+export const createAdminService = async (email: string) => {
+
+    const checkEmail = await prisma.user.findUnique({
+        where: {
+            email,
+            role: Role.ADMIN
+        }
+    });
+
+    if(checkEmail) {
+        throw new Error("El correo electrónico ya está registrado");
+    }
+
+    const { password, passwordHash } = await generateAdminPassword();
+
+    await prisma.user.create({
+        data: {
+            email,
+            passwordHash: passwordHash,
+            role: Role.ADMIN,
+            isActive: true
+        }
+    });
+
+    sendEmail({
+        to: email,
+        subject: "Cuenta de administrador creada",
+        html: `<p>Se ha creado una cuenta de administrador para usted. Su contraseña temporal es: <strong>${password}</strong>. Por favor, cambie su contraseña después de iniciar sesión.</p>`
+    });
+
+    return 'Administrador creado exitosamente';
+
+}
